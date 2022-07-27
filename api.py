ce80c774d086505e44acba060c0f2b8c1aebddd0
Juan Nicolás Barreto Parra <jnbarretop@correo.udistrital.edu.co>
2022-07-27 17:37:37 -0500

    nuevo commit

--- api.py
+++ api.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI, fastAPI
+from msilib.schema import tables
+from fastapi import FastAPI
 from app import App
 
 app= FastAPI()
 
+tables= []
 @app.get("/FRANI")
 def hola():
 
     miApp=App()
 
     return{miApp.ingresardatos()}
+
+@app.get('/tables')
+def get_tablas():
+    miApp=App()
+    return{miApp.ingresardatos()}
+
