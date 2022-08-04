9f9fa9f0c4b53c0488ce3b6ae01d383290f397f0
Juan Nicolás Barreto Parra <jnbarretop@correo.udistrital.edu.co>
2022-08-04 09:51:38 -0500

    nuevo commit

--- bases de datos2.py
+++ bases de datos2.py
@@ -6,15 +6,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import column, Integer, string, DateTime
 
-engine = create_engine('postgresql://')
+engine = create_engine('mysql+pymysql://root:Fr@ani12345@localhost=3306')
 Base= declarative_base()
 
 class user(Base):
     __tablename__= 'users'
 
     id= column (Integer(), primary_key=True)
-    username= Column(String(50),nullable=False, unique=True)
-    email= Column (String(50), nullable=False, unique=True)
+    genero= Column(String(50),nullable=False, unique=True)
+    idioma= Column (String(50), nullable=False, unique=True)
     created_at = Column(DateTime(), default=datetime.now())
 
     def __str__(self):
